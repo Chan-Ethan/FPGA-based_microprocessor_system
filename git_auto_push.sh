85311686ee61135b0fb5f6f7aff52a768003c338
Ethan_linux <wsqlbh@163.com>
2025-02-02 01:00:38 +0800

    add

---
+++ git_auto_push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+message="[linux_auto] bug fixed"
+echo $message
+while getopts 'm:M:' opt
+do
+    case $opt in
+        m|M)    message="$OPTARG";;
+        *)      echo "[Error] illegal option input"
+                exit;;
+    esac
+done
+
+git add -u
+git commit -m $message
+git push
