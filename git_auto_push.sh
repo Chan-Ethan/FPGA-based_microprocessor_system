9215b2882e528d964587e47cd6cc621951672a1a
Ethan_PC <wsqlbh@163.com>
2025-03-02 17:25:45 +0000

    [TEST] connect mouse to data bus, add mouse position calculation

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 message="[linux_auto] bug fixed"
-echo $message
 while getopts 'm:M:' opt
 do
     case $opt in
@@ -11,6 +10,7 @@ do
     esac
 done
 
+echo $message
 git add -u
 git commit -m "$message"
 git push
